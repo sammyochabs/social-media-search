182db6cf8556a5377389c23ec1f2ccd5088b1a91
sammyochabs <sammymadgenius@gmail.com>
2021-06-30 06:09:49 +0100

    initial

---
+++ pages/_document.js
@@ -0,0 +1,53 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+            crossOrigin="anonymous"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;1,200;1,300&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+            integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+            crossOrigin="anonymous"
+            referrerpolicy="no-referrer"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+
+          <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+            integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+            crossOrigin="anonymous"
+          ></script>
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+            integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+            crossOrigin="anonymous"
+          ></script>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+async function handler(req, res) {
+  req.body = JSON.parse(req.body);
+  let term = req.body.searchValue;
+  const url = `https://api.twitter.com/2/tweets/search/recent?query=${term}&max_results=100&expansions=attachments.media_keys,author_id&user.fields=username&media.fields=url`;
+  const resp = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer AAAAAAAAAAAAAAAAAAAAAF9IRAEAAAAAlvZ5FFM5tappFincPtqWODRDKBs%3DnZHFpBXBUy3ftBXs2GdehndKIgZuf4rBD3sB7e0eRg9cBGXgXv",
+      "Access-Control-Allow-Origin": "*",
+    },
+    redirect: "follow",
+  });
+  const tweets = await resp.json();
+
+  const vidResp = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&key=AIzaSyBZf_-RivsmpSnTvb7EOL1Ma303ovwNI3s&q=${term}`,
+    {
+      method: "GET",
+      redirect: "follow",
+    }
+  );
+
+  const videos = await vidResp.json();
+
+  res.status(200).json({ tweets, videos });
 }
+
+export default handler;
