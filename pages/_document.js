2a76d25243bc9e42716a5d765953588f47fdb6fc
sammyochabs <sammymadgenius@gmail.com>
2021-06-30 06:27:25 +0100

    initial

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import SCript from next/script;
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -29,20 +30,23 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
             referrerpolicy="no-referrer"
           />
-          <script
-            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-            integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-            crossOrigin="anonymous"
-          ></script>
-          <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-            integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-            crossOrigin="anonymous"
-          ></script>
+        
         </Head>
         <body>
           <Main />
           <NextScript />
+
+
+          <Script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+            integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+            crossOrigin="anonymous"
+          ></Script>
+          <Script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+            integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+            crossOrigin="anonymous"
+          ></Script>
         </body>
       </Html>
     );
